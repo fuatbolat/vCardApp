70ec903ba8022e4a90fa6e84b4ef9550dd594701
fuatbolat <byfuatbolat@gmail.com>
2024-10-18 23:41:36 +0300

    Initial Commit

---
+++ vCardAppTests/vCardAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  vCardAppTests.swift
+//  vCardAppTests
+//
+//  Created by Fuat Bolat on 18.10.2024.
+//
+
+import Testing
+@testable import vCardApp
+
+struct vCardAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
