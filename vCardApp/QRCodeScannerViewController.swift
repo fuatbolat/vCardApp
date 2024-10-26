375f308df879ba4034b009c5b74a313ce21826a7
fuatbolat <byfuatbolat@gmail.com>
2024-10-26 17:41:14 +0300

    main

---
+++ vCardApp/QRCodeScannerViewController.swift
@@ -0,0 +1,88 @@
+//
+//  QRCodeScannerViewController.swift
+//  vCardApp
+//
+//  Created by Fuat Bolat on 26.10.2024.
+//
+
+import UIKit
+import AVFoundation
+
+class QRCodeScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        captureSession = AVCaptureSession()
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if captureSession.canAddOutput(metadataOutput) {
+            captureSession.addOutput(metadataOutput)
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject, let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            
+            // QR verisini ayrıştır
+            parseQRCodeData(stringValue)
+        }
+
+        dismiss(animated: true)
+    }
+
+    func parseQRCodeData(_ data: String) {
+        let components = data.components(separatedBy: ",")
+        var userInfo: [String: String] = [:]
+
+        for component in components {
+            let keyValue = component.components(separatedBy: ":")
+            if keyValue.count == 2 {
+                userInfo[keyValue[0]] = keyValue[1]
+            }
+        }
+
+        // Dijital kart ekranını göster
+        if let name = userInfo["Name"], let number = userInfo["Number"], let email = userInfo["Email"] {
+            let digitalCardVC = DigitalCardViewController()
+            digitalCardVC.userName = name
+            digitalCardVC.userNumber = number
+            digitalCardVC.userEmail = email
+            present(digitalCardVC, animated: true)
+        }
+    }
+}
+
--- vCardApp/ViewController.swift
+++ vCardApp/ViewController.swift
@@ -6,14 +6,92 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+   
+    
+   
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.layer.cornerRadius = imageView.frame.height / 2
+        imageView.clipsToBounds = true
+        
+        let name = UserDefaults.standard.string(forKey: "userName") ?? "İsim bulunamadı"
+            let number = UserDefaults.standard.string(forKey: "userNumber") ?? "Numara bulunamadı"
+            let email = UserDefaults.standard.string(forKey: "userEmail") ?? "Email bulunamadı"
+        nameTextField.text=name
+        numberTextField.text=number
+        emailTextField.text=email
+        
+        
     }
+    //when the perform seque
+    override func viewDidDisappear(_ animated: Bool) {
+        nameTextField.text = ""
+        numberTextField.text = ""
+        emailTextField.text = ""
+    }
+    
+    @IBAction func addPhotoButton(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = .photoLibrary // Fotoğraf kütüphanesinden seçim yapacağız.
+                imagePicker.allowsEditing = false // Kullanıcının resmi düzenlemesine izin veriyoruz (isteğe bağlı)
+                
+                present(imagePicker, animated: true, completion: nil)
+           
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+           if let selectedImage = info[.originalImage] as? UIImage {
+               imageView.image = selectedImage // Seçilen resmi imageView'da gösteriyoruz
+           }
+           picker.dismiss(animated: true, completion: nil) // Picker'ı kapat
+       }
+       
+       // Kullanıcı resmi seçmeyi iptal ederse
+       func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+           picker.dismiss(animated: true, completion: nil) // Picker'ı kapat
+       }
+    
+    
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+               let name = nameTextField.text ?? ""
+               let number = numberTextField.text ?? ""
+               let email = emailTextField.text ?? ""
+     
+               
+               if let image = imageView.image, let imageData = image.jpegData(compressionQuality: 0.8) {
+                   UserDefaults.standard.set(imageData, forKey: "userPhoto")
+               }
+        else{print("noprofilephotoselected")}
 
+               
+               UserDefaults.standard.set(name, forKey: "userName")
+               UserDefaults.standard.set(number, forKey: "userNumber")
+               UserDefaults.standard.set(email, forKey: "userEmail")
+       
+        
+        performSegue(withIdentifier: "goToSecondViewController", sender: self)
+        
+        
+           
+    }
+    
+   
+   
 
+    
 }
+
 
